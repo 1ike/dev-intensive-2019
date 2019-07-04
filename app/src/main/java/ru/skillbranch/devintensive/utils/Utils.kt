d83c9bd8aadd3b60d7a5d81a6494984d174abb7a
1ike <googlohren@gmail.com>
2019-07-04 19:57:17 +0300

    Add trim and ifEmpty to parseFullName.

--- app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
+++ app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
@@ -2,10 +2,10 @@ package ru.skillbranch.devintensive.utils
 
 object Utils {
     fun parseFullName(fullName: String?) : Pair<String?, String?> {
-        val parts : List<String>? = fullName?.split(' ')
+        val parts : List<String>? = fullName?.trim()?.split(' ')
 
-        val firstName  = parts?.getOrNull(0)
-        val lastName  = parts?.getOrNull(1)
+        val firstName  = parts?.getOrNull(0)?.ifEmpty { null }
+        val lastName  = parts?.getOrNull(1)?.ifEmpty { null }
 
         return Pair(firstName, lastName)
     }
